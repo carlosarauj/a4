42f189bde2281b871aaec8da2072a15dfa8cb7e6
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-04-22 19:34:03 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,10 @@
+salario = float(input("informe seu salário: "))
+
+if salario <= 1400:
+    print("tá lascado")
+elif salario > 1400 and salario < 3000:
+    print("ta indo")
+elif salario > 3000:
+    print("tá bem")
+
+#usa o terminal com: python nomedoarquivo.py
